947468508e6f7e68b2217b22ea9a842901c2bd4a
Emilqwe <emomolo20@gmail.com>
2021-09-22 09:55:40 +0200

    Plik

--- Pliki/Form1.Designer.cs
+++ Pliki/Form1.Designer.cs
@@ -29,13 +29,65 @@ namespace Pliki
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.wyswietl = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.otworz = new System.Windows.Forms.Button();
+            this.zapisz = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // wyswietl
+            // 
+            this.wyswietl.Location = new System.Drawing.Point(12, 21);
+            this.wyswietl.Multiline = true;
+            this.wyswietl.Name = "wyswietl";
+            this.wyswietl.Size = new System.Drawing.Size(547, 369);
+            this.wyswietl.TabIndex = 0;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // otworz
+            // 
+            this.otworz.Location = new System.Drawing.Point(649, 32);
+            this.otworz.Name = "otworz";
+            this.otworz.Size = new System.Drawing.Size(75, 23);
+            this.otworz.TabIndex = 1;
+            this.otworz.Text = "Otwórz";
+            this.otworz.UseVisualStyleBackColor = true;
+            this.otworz.Click += new System.EventHandler(this.otworz_Click);
+            // 
+            // zapisz
+            // 
+            this.zapisz.Location = new System.Drawing.Point(649, 311);
+            this.zapisz.Name = "zapisz";
+            this.zapisz.Size = new System.Drawing.Size(75, 23);
+            this.zapisz.TabIndex = 2;
+            this.zapisz.Text = "Zapisz";
+            this.zapisz.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.zapisz);
+            this.Controls.Add(this.otworz);
+            this.Controls.Add(this.wyswietl);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox wyswietl;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button otworz;
+        private System.Windows.Forms.Button zapisz;
     }
 }
 
--- Pliki/Form1.cs
+++ Pliki/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Pliki
 {
@@ -15,6 +16,43 @@ namespace Pliki
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void otworz_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            OpenFileDialog Dialog = new OpenFileDialog();
+            
+            Dialog.InitialDirectory = "c:\\";
+            Dialog.Title = "Open text files";
+            Dialog.Filter = "txt files|*.txt|All files|*.*";
+            Dialog.FilterIndex = 2;
+            Dialog.RestoreDirectory = true;
+
+            if (Dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = Dialog.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            wyswietl.Text = Dialog.
+                        }
+                    }
+                }
+                catch(Exception ex)
+                {
+
+                }
+            }
+
+
         }
     }
 }
